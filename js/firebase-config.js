2b4003c2fb9ef34d85f60b20b0505a5589223656
eshwar-iiti <me240003029@iiti.ac.in>
2026-01-06 17:25:59 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,12 @@
 // Firebase configuration
 // Replace the placeholders with your actual Firebase project credentials
 const firebaseConfig = {
-  apiKey: "AIzaSyBU-yYugzdcy0Px9s4Mxs_VbLfdmEk1ASU",
-  authDomain: "spi-cpi.firebaseapp.com",
-  projectId: "spi-cpi",
-  storageBucket: "spi-cpi.firebasestorage.app",
-  messagingSenderId: "641411211734",
-  appId: "1:641411211734:web:91ef314c54a4d402ba4477",
-  measurementId: "G-F9DKRYXW7E"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
 };
 
 // Initialize Firebase
